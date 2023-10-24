904171fd1684c829b78367eaebc0c7b300598c1d
Dean <dean96@live.co.uk>
2023-10-24 11:22:52 +0100

    4 show list of all articles

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -1,6 +1,37 @@
+import {useParams} from 'react-router-dom';
+import {useState, useEffect} from 'react';
+import axios from 'axios';
+
 function Articles() {
+    const params = useParams();
+    console.log(params);
+    const [articles, setArticles] = useState([]);
+    //create base url
+    useEffect(() => {
+        axios.get('https://news-api-9k2x.onrender.com/api/articles', {params: {topic: params.topic}})
+        .then(({data}) => {
+            setArticles(data.articles);
+        })
+    },[])
+console.log(articles)
     return (
-        <p>Articles</p>
+        <>
+            <p>Articles {params.topic}</p>
+            <ol>
+                {articles.map(article => {
+                    return (
+                        <li key={`${article.article_id} ${article.title}`} article_id={article.article_id}>
+                            <p>{article.title}</p>
+                            <p>{article.created_at}</p>
+                            <p>{article.topic}</p>
+                            <p>{article.author}</p>
+                        </li>
+                    )
+                })}
+            </ol>
+        </>
+        
+
     )
 }
 
