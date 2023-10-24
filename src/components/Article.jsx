4c99743d30d3493f46f69bfec564b144f8bff158
Dean <dean96@live.co.uk>
2023-10-24 20:44:53 +0100

    implimented posting comment to article

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,4 +1,4 @@
-import {useEffect, useState} from 'react';
+import {useEffect, useState, Component} from 'react';
 import {useParams} from 'react-router-dom';
 import axios from 'axios';
 import Comment from './Comment';
@@ -15,6 +15,10 @@ function Article() {
     const [articleVotes, setArticleVotes] = useState(0);
     const [page, setPage] = useState(1);
     const [limit, setLimit] = useState(5);
+    const [commentInput, setCommentInput] = useState('');
+    const [commentPosted, setIsCommentPosted] = useState(false);
+    const [isPostingComment, setIsPostingComment] = useState(false);
+
     useEffect(() => {
         setLoadingArticles(true);
         axios.get(`https://news-api-9k2x.onrender.com/api/articles/${params.article_id}`)
@@ -28,6 +32,7 @@ function Article() {
         })
     }, [])
     useEffect(() => {
+        setIsCommentPosted(false);
         setLoadingComments(true);
         axios.get(`https://news-api-9k2x.onrender.com/api/articles/${params.article_id}/comments?p=${page}&limit=${limit}`)
         .then(({data: {comments: requestedComments}}) => {
@@ -37,12 +42,25 @@ function Article() {
             setLoadingComments(false);
             console.log(err);
         })
-    },[page, limit])
+    },[page, limit, commentPosted])
+    console.log('hi')
     const articleId = article.article_id;
     const voter = 'article';
+    function handleSubmit(e) {
+        e.preventDefault();
+        setIsPostingComment(true);
+        axios.post(`https://news-api-9k2x.onrender.com/api/articles/${articleId}/comments`, {body: commentInput, username: 'grumpy19'}).then(() => {
+            setIsPostingComment(false);
+            setCommentInput('')
+            setIsCommentPosted(true);
+        }).catch(err => {
+            setIsPostingComment(false);
+            console.log(err)
+        })
+    }
     return (
         <>
-            <p>{loadingArticles || loadingComments? 'Loading...': ''}</p>
+            <p>{loadingArticles || loadingComments ? 'Loading...': ''}</p>
             <div>
                 <h2>{article.title}</h2>
                 <p>{article.topic}</p>
@@ -55,11 +73,14 @@ function Article() {
                 <button disabled={voted < 0} onClick={(e) => {handleVote(e, setVoted, articleId, voter, setArticleVotes)}}>v</button>
                 <p>{articleVotes}</p>
             </div>
-            <form>
-                <label htmlFor="add-coment">Add comment</label>
-                <input id="add-comment" name="add-comment"></input>
+            {isPostingComment ? 'Loading...' : 
+            <form onSubmit={handleSubmit}>
+                <p>userAvatar</p>
+                <label htmlFor="add-comment">Add comment</label>
+                <input id="add-comment" value={commentInput} onChange={(e) => {setCommentInput(e.target.value)}} name="add-comment"></input>
                 <button type="submit" placeholder="add comment">submit</button>
             </form>
+            }
             <ul>
                 {comments.map(comment => {
                     return (
