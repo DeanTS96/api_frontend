f48b25aec5108f9336a5f163727f1eba79e47627
Dean <dean96@live.co.uk>
2023-10-25 20:21:16 +0100

    added sign in, profile and sign up page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { createContext, useState } from 'react';
 import './App.css';
 import Nav from './components/Nav';
 import Articles from './components/Articles';
@@ -10,22 +10,27 @@ import Profile from './components/Profile';
 import Article from './components/Article';
 import {Routes, Route} from 'react-router-dom';
 
+export const UserContext = createContext(null);
+
 function App() {
 
+  const [user, setUser] = useState({});
   return (
     <>
-    <h1>NC News</h1>
-    <Nav/>
-    <Routes>
-      <Route path="/" element={<Articles/>}></Route>
-      <Route path="/articles/:article_id" element={<Article/>}></Route>
-      <Route path="/topics" element={<Topics/>}></Route>
-      <Route path="/create_article" element={<CreateArticle/>}></Route>
-      <Route path="/sign-in" element={<SignIn/>}></Route>
-      <Route path="/sign-up" element={<SignUp/>}></Route>
-      <Route path="/profile" element={<Profile/>}></Route>
-      <Route path="/topics/:topic" element={<Articles/>}></Route>
-    </Routes>
+    <UserContext.Provider value={{user, setUser}}>
+      <h1>NC News</h1>
+      <Nav/>
+      <Routes>
+        <Route path="/" element={<Articles />}></Route>
+        <Route path="/articles/:article_id" element={<Article />}></Route>
+        <Route path="/topics" element={<Topics />}></Route>
+        <Route path="/create_article" element={<CreateArticle />}></Route>
+        <Route path="/sign-in" element={<SignIn setUser={setUser} />}></Route>
+        <Route path="/sign-up" element={<SignUp />}></Route>
+        <Route path="/profile" element={<Profile />}></Route>
+        <Route path="/topics/:topic" element={<Articles />}></Route>
+      </Routes>
+    </UserContext.Provider>
     </>
   )
 }
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,6 +1,31 @@
+import {useState, useNavigate} from 'react';
+
 function SignUp() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [avatarURL, setAvatarURL] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(e.target.value);
+        //setIsLoading(true);
+        //axios.post(`https://news-api-9k2x.onrender.com/api/users/${e.target[0].value}`, {body: commentInput, username: 'grumpy19'}).then(x => {console.log(x);})
+    }
     return (
-        <p>Signup</p>
+        <>
+            {isLoading ? 'Loading...' : ''}
+            <p>Signup</p>
+            <form>
+                <label htmlFor="username">Username: </label>
+                <input id="username" name="username" value={username} onChange={(e)=>{setUsername(e.target.value)}}/>
+                <label htmlFor="password">Password: </label>
+                <input id="password" name="password" value={password} onChange={(e)=>{setPassword(e.target.value)}}/>
+                <label htmlFor="avatar-url">Avatar: </label>
+                <input id="avatar-url" name="avatar-rul" value={avatarURL} onChange={(e)=>{setAvatarURL(e.target.value)}}/>
+                <button type="submit" onSubmit={(e)=>{handleSubmit(e)}}>Submit</button>
+            </form>
+        </>
     )
 }
 
